3d0ffb8b4245d6936b2f92deeecbc9e03d18f3c0
leightonb <leightonb@live.com>
2025-02-21 20:32:05 +1000

    added inputs for login

--- Ui/src/main.ts
+++ Ui/src/main.ts
@@ -6,9 +6,26 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import '@mdi/font/css/materialdesignicons.css' 
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: 'mdi', // This is already the default value - only for display purposes
+      },
+  })
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
