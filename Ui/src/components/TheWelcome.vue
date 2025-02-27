8203f30d0220c0288942fbfa4f5fba130e2e99d4
leightonb <leightonb@live.com>
2025-02-27 21:57:34 +1000

    update

--- Ui/src/components/TheWelcome.vue
+++ Ui/src/components/TheWelcome.vue
@@ -2,20 +2,12 @@
 import { useUserStore } from '@/stores/user'
 
 import { ref } from 'vue'
-const todos = ref([])
-const username = ref('');
+const username = ref('')
 const password = ref('')
 const userStore = useUserStore()
-function getTodoItems() {
-  fetch('http://localhost:5171/todoitems')
-    .then((response) => response.json())
-    .then((data) => {
-      todos.value = data
-    })
-}
 
 async function login() {
-  console.log(username, password);
+  console.log(username, password)
   const rawResponse = await fetch('http://localhost:5171/login', {
     method: 'POST',
     headers: {
@@ -31,19 +23,12 @@ async function login() {
   userStore.setUser(json.userDto)
   userStore.setToken(json.token)
 }
-
-function logItems() {
-  console.log(userStore.getUser(), userStore.getToken())
-}
 </script>
 
 <template>
   <v-text-field label="Username" v-model="username"></v-text-field>
   <v-text-field label="Password" v-model="password" type="password"></v-text-field>
   <v-btn text="Login" @click="login"></v-btn>
-    <button @click="login">Login</button>
-    <button @click="getTodoItems">Get Items</button>
-    <button @click="logItems">Log Items</button>
-    <br />
+  <br />
 </template>
 <script lang="ts"></script>
